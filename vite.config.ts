33e1f8a52024c2648990212d272f227f07583a1b
bertjee321 <b.kruiter.91@gmail.com>
2025-01-15 21:15:19 +0100

    Remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,4 @@ export default defineConfig({
     host: '0.0.0.0', // Allow access from your local network
     port: 5173,      // Optional: Specify the port
   },
-  base: '/caravanstalling-ter-apel-frontend/', // Optional: Specify the base path
 })
